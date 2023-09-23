f009fb6bd95f3748915088a275b394933e50b391
jannatulferdous <jannatulferdausirin1@gmail.com>
2023-09-23 18:16:41 +0300

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'bannerImage': "url('images/Rectangle 1 1.png')",
+                'climbTogetherImage': "url('../images/image 2 1.jpg')"
+
+
+            },
+
+            colors: {
+                blackColor: '#000000',
+                gold: '#FBD784',
+                darkGold: '#FFA343',
+                white: '#FFFFFF',
+                offWhite: ' #FFF',
+                lightGold : '#FFD1A0'
+
+            },
+            fontFamily: {
+                'openSans': ['Open Sans', 'sans - serif',]
+
+            }
+
+        }
+    }
+}
